b9507b86fb7381c169ef1173df65f1530ed934ff
uttam282005 <uttam282005@gmail.com>
2025-02-12 21:53:34 +0530

    generic functions

--- lec-04/generic.c
+++ lec-04/generic.c
@@ -1,5 +1,19 @@
+#include <stddef.h>
 #include <stdio.h>
 
-//+-- 1 lines:{
-// int main() { printf("Jichar *, ..."); }
-//}
+int cmp(void *elem, void *arg) {
+  int tocheck = *((int *)elem);
+  int key = *((int *)arg);
+
+  return tocheck == key ? 1 : 0;
+}
+
+int generic_count(void *key, void *arr, size_t elemsz, size_t n, int *cmp()) {
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    void *current = arr + i * elemsz;
+    if (*cmp(current, key) == 1) {
+      cnt++;
+    }
+  }
+}
